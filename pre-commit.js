fc5cc926a5d3745227a5e7bd2f9f80d6d7c0e2b0
maxKharkovenko <maxkharkovenko@gmail.com>
2020-03-28 20:09:44 +0200

    a

--- pre-commit.js
+++ pre-commit.js
@@ -1,10 +1,20 @@
+const { exec } = require('child_process');
+const opts = { cwd: __dirname };
+const cmd = 'diff -qr client/src/shared server/src/shared';
+
 async function run() {
- const a = 3;
- if (a > 4) {
-   console.log('succes data');
- } else {
-   console.log('you can not commit thi changes becouse file is difference');
-   throw "Error2";
- }
+  const a = 3;
+  if (a > 2) {
+    console.log('succes data');
+  } else {
+    console.log('you can not commit thi changes becouse file is difference');
+    throw "Error2";
+  }
 }
-run();
+
+exec(run, opts, (err, stdout, stderr) => {
+  if (err) {
+    process.exitCode = 1;
+    console.log('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF');
+  }
+});
