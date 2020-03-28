3c129902c92410f4cf83e86f084c59a65a6f16a5
maxKharkovenko <maxkharkovenko@gmail.com>
2020-03-28 20:02:16 +0200

    a

--- pre-commit.js
+++ pre-commit.js
@@ -1,4 +1,10 @@
 async function run() {
-  throw "Error2";
+ const a = 3;
+ if (a > 4) {
+   console.log('succes data');
+ } else {
+   console.log('you can not commit thi changes becouse file is difference');
+   throw "Error2";
+ }
 }
 run();
